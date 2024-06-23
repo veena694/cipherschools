36963d501a9fcdb03638e2ec2cb71b790d34d3c9
veena694 <119649485+veena694@users.noreply.github.com>
2024-06-23 16:05:50 +0530

    Add files via upload

--- lecture18/src/screens/ToDoScreen.js
+++ lecture18/src/screens/ToDoScreen.js
@@ -1,1 +1,9 @@
+import { Component } from "react";
+class ToDoScreen extends Component{
+    render(){
+        return <h1 className="ui heading center">ToDo List </h1>;
+        
+    }
 
+}
+export default ToDoScreen;
