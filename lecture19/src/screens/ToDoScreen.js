552e039ce3028b2ddee402a53f5878e244fd2379
veena694 <119649485+veena694@users.noreply.github.com>
2024-06-24 17:00:58 +0530

    Add files via upload

--- lecture19/src/screens/ToDoScreen.js
+++ lecture19/src/screens/ToDoScreen.js
@@ -1,1 +1,30 @@
+import { Component } from "react";
+class ToDoScreen extends Component{
+    state= {
+        taskCount: 0,
 
+    };
+    
+    render(){
+        return (
+            <div className="screen">
+                <h1 className="ui heading center">ToDo List </h1>
+                <div>
+                    <button onClick={(e)=>{
+                         this.setState({
+                            ...this.state,
+                            taskCount: this.state.taskCount + 1,
+                        });
+                        console.log(this.state.taskCount);
+                    }} 
+                    className="ui secondary button">Add Task</button>
+                    <p>The number of task:{ this.state.taskCount}</p>
+                </div>
+            </div>
+        
+        )
+        
+    }
+
+}
+export default ToDoScreen;
