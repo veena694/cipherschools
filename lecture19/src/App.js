2b563b90b005dbe575ecbd34e0702af7ce1c860a
veena694 <119649485+veena694@users.noreply.github.com>
2024-06-24 16:59:14 +0530

    Add files via upload

---
+++ lecture19/src/App.js
@@ -0,0 +1,11 @@
+import { Component } from "react";
+import ToDoScreen from "./screens/ToDoScreen";
+class App extends Component{
+    render() {
+        return(
+            <ToDoScreen/>
+        )
+
+    }
+}
+export default App;
