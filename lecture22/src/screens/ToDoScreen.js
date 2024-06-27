ad98e94a3dcd927712c514543493f4d7a74ea1d4
veena694 <119649485+veena694@users.noreply.github.com>
2024-06-27 18:39:05 +0530

    Add files via upload

--- lecture22/src/screens/ToDoScreen.js
+++ lecture22/src/screens/ToDoScreen.js
@@ -1,1 +1,38 @@
+import {useState} from "react";
+import Task from "../components/Task";
+import AddTask from "../components/AddTask";
 
+function ToDoScreen(){
+    const [taskList, setTaskList]= useState([]);
+    return(
+        <>
+            <div className="screen">
+                <h1 className="ui heading center">To Do List</h1>
+                <div onClick={(e)=>{
+                    setTaskList([
+                        ...taskList,
+                        {
+                            title: "Go to Gym",
+                            description: "going to gym is goood for muscle growth",
+                            create_date: new Date(),
+                        }
+                    ]);
+                    console.log(taskList);
+                }}
+                className="ui secondary button">
+                    Add Task
+                </div>
+                <section>
+                    <div className="ui cards">
+                        {taskList.map((task) => (
+                            <Task task={task}/>
+                            ))}
+                    </div>
+                </section>
+                <AddTask/> 
+            </div>
+            
+        </>
+    );
+}
+export default ToDoScreen;
