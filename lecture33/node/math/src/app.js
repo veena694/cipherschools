75138d1cffc683bd0924873c58f56c70dafe9601
veena694 <119649485+veena694@users.noreply.github.com>
2024-06-30 18:09:46 +0530

    Add files via upload

--- lecture33/node/math/src/app.js
+++ lecture33/node/math/src/app.js
@@ -1,1 +1,26 @@
+// const http = require("http");
 
+// const server = http.createServer((req, res)=>{
+//   res.write("This is some response from your first Node.js server");
+//   res.end();
+// });
+
+// server.listen(8080,() => {
+//  console.log("Server is running");
+// });
+const express = require("express");
+const app = express();
+
+app.get("/",(req,res)=>{
+  res.send("This is some response from your first Node.js server")
+})
+
+app.get("/add", (req, res)=>{
+  let{a: firstNumber,b:SecondNumber} = req.query;
+  let sum = parseInt(firstNumber) + parseInt(SecondNumber);
+  res.send({sum});
+}) 
+
+app.listen(8080, ()=>{
+  console.log("Sever is running");
+})
