77533d74d3873e8923b3a5f20711f50d883fe361
veena694 <119649485+veena694@users.noreply.github.com>
2024-07-06 15:08:35 +0530

    Add files via upload

--- lecture37/src/appMongoose.js
+++ lecture37/src/appMongoose.js
@@ -1,1 +1,12 @@
-
+const {connect} = require("mongoose");
+const MONGO_URL= "mongodb+srv://vanshika:vanshu@cluster0.eg26iku.mongodb.net";
+const DB_NAME=`cs-mern`;
+async function connectDb(){
+  try{
+    await connect(`${MONGO_URL}/${DB_NAME}`);
+    console.log(`MongoDb connected.`);
+  }catch(err){
+    console.error(err);
+  }
+}
+connectDb();
